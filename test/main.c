4d44143096999e5d04bb08ef4b1993ae02387632
Daniel Thalmann <daniel@thalmann.li>
2022-06-17 10:10:58 +0200

    update test

--- test/main.c
+++ test/main.c
@@ -1,21 +1,37 @@
 
 #include "mlx.h"
+#include <stdio.h>
+
+typedef struct s_app
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+} t_app;
+
+int	loop(t_app *app)
+{
+	mlx_clear_window(app->mlx_ptr, app->win_ptr);
+	for (size_t i = 0; i < 100; i++)
+	{
+		mlx_pixel_put(app->mlx_ptr, app->win_ptr, i + 50, 100, 512);
+	}
+	return (0);
+}
 
 int main(int argc, char** argv)
 {
-	void	*mlx_ptr;
+	t_app	app;
 
-	void	*mlx_win;
-
-	(void) mlx_win;
 	(void) argc;
 	(void) argv;
 
-	mlx_ptr = mlx_init();
+	app.mlx_ptr = mlx_init();
 
-	mlx_win = mlx_new_window(mlx_ptr, 640, 480, "test window");
+	app.win_ptr = mlx_new_window(app.mlx_ptr, 640, 480, "test window");
+
+	mlx_loop_hook (app.mlx_ptr, &loop, &app);
 	
-	mlx_loop(mlx_ptr);
+	mlx_loop(app.mlx_ptr);
 	
 	return (0);
 }
