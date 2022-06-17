1daad99a93284c73cd2286cae55e03ff64037d20
Daniel Thalmann <daniel@thalmann.li>
2022-06-17 19:43:14 +0200

    fix color create

--- srcs/mlx_color.c
+++ srcs/mlx_color.c
@@ -12,9 +12,9 @@ SDL_Color	color_create(int color)
 {
 	SDL_Color	c;
 
-	c.b = (color & 0xFF) / 255.0;
-	c.g = ((color >> 8) & 0xFF) / 255.0;
-	c.r = ((color >> 16) & 0xFF) / 255.0;
-	c.a = 255;
+	c.b = (color & 0xFF);
+	c.g = ((color >> 8) & 0xFF);
+	c.r = ((color >> 16) & 0xFF);
+	c.a = SDL_ALPHA_OPAQUE;
 	return (c);
 }
