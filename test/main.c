e4ebc50e951f73b4cde73a45513bc2c0db4c4940
Daniel Thalmann <daniel@thalmann.li>
2022-06-19 08:57:27 +0200

    test mlx_put_image_to_window

--- test/main.c
+++ test/main.c
@@ -6,7 +6,13 @@ typedef struct s_app
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+
 	void	*img_ptr;
+	int		*img_pixel_ptr;
+	int		bits_per_pixel;
+	int		size_line; 
+	int		endian;
+
 } t_app;
 
 int	loop(t_app *app)
@@ -16,7 +22,7 @@ int	loop(t_app *app)
 	{
 		mlx_pixel_put(app->mlx_ptr, app->win_ptr, i + 50, 100, 0xFF0000);
 	}
-	mlx_put_image_to_window(app->mlx_ptr, app->win_ptr, app->img_ptr, 10, 110);
+	mlx_put_image_to_window(app->mlx_ptr, app->win_ptr, app->img_ptr, 5, 10);
 
 	return (0);
 }
@@ -32,16 +38,19 @@ int main(int argc, char** argv)
 
 	app.win_ptr = mlx_new_window(app.mlx_ptr, 640, 480, "test window");
 
-	app.img_ptr = mlx_new_image(app.mlx_ptr, 20, 20);
+	app.img_ptr = mlx_new_image(app.mlx_ptr, 100, 100);
 
-//	for (size_t i = 0; i < 100; i++)
-//	{
-//		mlx_pixel_put(app->mlx_ptr, app->win_ptr, i + 50, 100, 0x00ff00);
-//	}	
+	app.img_pixel_ptr = (int *)mlx_get_data_addr(app.img_ptr, &app.bits_per_pixel,
+			   &app.size_line,  &app.endian);
+
+	for (size_t i = 0; i < 100; i++)
+		app.img_pixel_ptr[i] = 0x00ff00;
 
 	mlx_loop_hook (app.mlx_ptr, &loop, &app);
 	
 	mlx_loop(app.mlx_ptr);
+
+	mlx_destroy_image(app.mlx_ptr, app.img_ptr);
 	
 	return (0);
 }
