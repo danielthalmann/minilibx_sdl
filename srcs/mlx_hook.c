e197eaca7c7f5f8115234d0bdfe2ff5463eec18b
Daniel Thalmann <daniel@thalmann.li>
2022-06-22 11:41:40 +0200

    trait hook

--- srcs/mlx_hook.c
+++ srcs/mlx_hook.c
@@ -10,27 +10,27 @@
 
 int	mlx_mouse_hook (void *win_ptr, int (*funct_ptr)(), void *param)
 {
-	(void) win_ptr;
-	(void) funct_ptr;
-	(void) param;
+	((t_mlx_window *)win_ptr)->hooks[MLX_ON_BUTTON_PRESS].mask = 0;
+	((t_mlx_window *)win_ptr)->hooks[MLX_ON_BUTTON_PRESS].hook = funct_ptr;
+	((t_mlx_window *)win_ptr)->hooks[MLX_ON_BUTTON_PRESS].param = param;
 
 	return (0);
 }
 
 int	mlx_key_hook (void *win_ptr, int (*funct_ptr)(), void *param)
 {
-	(void) win_ptr;
-	(void) funct_ptr;
-	(void) param;
+	((t_mlx_window *)win_ptr)->hooks[MLX_ON_KEY_RELEASE].mask = 0;
+	((t_mlx_window *)win_ptr)->hooks[MLX_ON_KEY_RELEASE].hook = funct_ptr;
+	((t_mlx_window *)win_ptr)->hooks[MLX_ON_KEY_RELEASE].param = param;
 
 	return (0);
 }
 
 int	mlx_expose_hook (void *win_ptr, int (*funct_ptr)(), void *param)
 {
-	(void) win_ptr;
-	(void) funct_ptr;
-	(void) param;
+	((t_mlx_window *)win_ptr)->hooks[MLX_ON_EXPOSE].mask = 0;
+	((t_mlx_window *)win_ptr)->hooks[MLX_ON_EXPOSE].hook = funct_ptr;
+	((t_mlx_window *)win_ptr)->hooks[MLX_ON_EXPOSE].param = param;
 
 	return (0);
 }
@@ -38,11 +38,9 @@ int	mlx_expose_hook (void *win_ptr, int (*funct_ptr)(), void *param)
 int	mlx_hook(void *win_ptr, int x_event, int x_mask,
                  int (*funct)(), void *param)
 {
-	(void) win_ptr;
-	(void) x_event;
-	(void) x_mask;
-	(void) funct;
-	(void) param;
+	((t_mlx_window *)win_ptr)->hooks[x_event].mask = x_mask;
+	((t_mlx_window *)win_ptr)->hooks[x_event].hook = funct;
+	((t_mlx_window *)win_ptr)->hooks[x_event].param = param;
 
 	return (0);
 }
